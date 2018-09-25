8b07fbd1a3e0a306c9a26bfa8cf8d8e12309db1d
Maria Kalemanov <kalemanov@biochem.mpg.de>
2018-09-25 15:30:08 +0200

    Converted assertions to test using pytest.

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from dna import DNA
+import pytest
 
-try:
-    assert DNA('ATB')
-except ValueError:
-    pass
-assert DNA('GTC').complementary_sequence == DNA('CAG')
-assert DNA('ATc').complementary_sequence == DNA('TAG')
-dna = DNA('ACT')
 
-print('it worked!')
+def test_bad_sequence_raises_error():
+    with pytest.raises(ValueError):
+        DNA('ATB')
+
+
+def test_complimentary_sequence_works():
+    assert DNA('GTC').complementary_sequence == DNA('CAG')
+    assert DNA('ATc').complementary_sequence == DNA('TAG')
